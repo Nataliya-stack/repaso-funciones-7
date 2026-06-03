ffc3a064fc082cbef5844c520888c3f8d81437ae
Natalia <nataliya.yanovic@mail.ru>
2026-06-03 10:44:47 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const esPalindromo = (frase) => {
+    const fraseLimpia = frase.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?¿¡! ]/g, "");
+
+    const fraseAlReves = fraseLimpia.split("").reverse().join("");
+
+    return fraseLimpia === fraseAlReves;
+};
+
+const inputEl = document.getElementById("inputTexto");
+const btnEl = document.getElementById("btnCalcular");
+const resultEl = document.getElementById("resultadoTexto");
+
+btnEl.addEventListener("click", () => {
+    const texto = inputEl.value.trim();
+
+    if (texto === "") {
+        resultEl.textContent = "Error: ¡El campo no puede estar vacío!";        
+        resultEl.style.color = "red";
+        return; 
+    }
+
+    const resultado = esPalindromo(texto);
+    
+    if (resultado === true) {
+        resultEl.textContent = `¡Sí, es un palíndromo! ✔`;
+        resultEl.style.color = "white";
+    } else {
+        resultEl.textContent = `No, no es un palíndromo ❌`;
+        resultEl.style.color = "white";
+    }
+
+    inputEl.value = "";
+    inputEl.focus();
+});
